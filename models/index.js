4d021ad4f3f3f5472ac64f4e0d12b2638d32c454
J Maltby <jacksonmaltby@gmail.com>
2023-04-09 11:35:24 -0700

    Added table definitions.

--- models/index.js
+++ models/index.js
@@ -1,16 +1,26 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require('./Product.js');
+const Category = require('./Category.js');
+const Tag = require('./Tag.js');
+const ProductTag = require('./ProductTag.js');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const routes = require('./routes');
 const sequelize = require('./config/connection.js')
 // import sequelize connection
 
+const Product = require('./models/Product');
+const Category = require('./models/Category');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,8 +16,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  })
-});
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+
+    await sequelize.sync({ force: false });
+    console.log('All models were synchronized successfully.');
+
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
